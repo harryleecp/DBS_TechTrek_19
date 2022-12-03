a1d15dd8e8a39eb9e554b7bb21062657840de6fc
Ang Wei Feng (Ted) <aweifeis@gmail.com>
2022-12-03 16:08:41 +0800

    get single and all accounts

--- spring-backend/src/main/java/com/dbstt19/dbs_backend/controller/AccountController.java
+++ spring-backend/src/main/java/com/dbstt19/dbs_backend/controller/AccountController.java
@@ -1,12 +1,34 @@
 package com.dbstt19.dbs_backend.controller;
 
+import com.dbstt19.dbs_backend.model.BankAccount;
+import com.dbstt19.dbs_backend.model.request.AllAccountsRequest;
+import com.dbstt19.dbs_backend.model.request.SingleAccountRequest;
+import com.dbstt19.dbs_backend.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin
 public class AccountController {
 
+    private final AccountService accountService;
+
+    @PostMapping("/account")
+    public ResponseEntity<BankAccount> getOne(@RequestBody SingleAccountRequest request) throws Exception {
+        BankAccount account = accountService.getOne(request.userId(), request.accountId());
+        return ResponseEntity.ok(account);
+    }
+
+    @PostMapping("/account/list")
+    public ResponseEntity<List<BankAccount>> getAll(@RequestBody AllAccountsRequest request) {
+        List<BankAccount> accounts = accountService.getAll(request.userId());
+        return ResponseEntity.ok(accounts);
+    }
 }
--- spring-backend/src/main/java/com/dbstt19/dbs_backend/repo/AccountRepository.java
+++ spring-backend/src/main/java/com/dbstt19/dbs_backend/repo/AccountRepository.java
@@ -3,5 +3,11 @@ package com.dbstt19.dbs_backend.repo;
 import com.dbstt19.dbs_backend.model.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<BankAccount, Long> {
+    Optional<BankAccount> findBankAccountByUserIdAndAccountId(Long userId, Long accountId);
+
+    List<BankAccount> findAllByUserId(Long userId);
 }
