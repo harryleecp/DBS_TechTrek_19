a92be2784e3be802d945a12171b501f1824695af
Ang Wei Feng (Ted) <aweifeis@gmail.com>
2022-12-03 12:00:17 +0800

    setup mysql db config

--- spring-backend/src/main/java/com/dbstt19/dbs_backend/controller/AuthController.java
+++ spring-backend/src/main/java/com/dbstt19/dbs_backend/controller/AuthController.java
@@ -1,12 +1,12 @@
 package com.dbstt19.dbs_backend.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
 
-    @PostMapping("/token")
+    @GetMapping("/token")
     public String getToken(){
         return "Hello world";
     }
