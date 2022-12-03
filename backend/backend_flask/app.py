ca668855ea61147208462dcb93b66a6743125b57
yuqi <111860394+threalyn@users.noreply.github.com>
2022-12-03 12:09:40 +0800

    Update app.py

--- backend/backend_flask/app.py
+++ backend/backend_flask/app.py
@@ -1,2 +1,9 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:letmeinpls@test-1.c6htqhzutcbg.ap-northeast-1.rds.amazonaws.com:3306/Bank-flask'
+db = SQLAlchemy(app)
+
+
