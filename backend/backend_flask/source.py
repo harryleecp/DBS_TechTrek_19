03f2184865a072c49e4c8919d630557afdc81ea7
LaddieTJC <103995451+LaddieTJC@users.noreply.github.com>
2022-12-03 16:08:05 +0800

    Add files via upload
    Working request

--- backend/backend_flask/source.py
+++ backend/backend_flask/source.py
@@ -12,8 +12,8 @@ api = Api(app)
 'mysql://admin:letmeinpls@database.ap-northeast-1.rds.amazonaws.com:3306/mydatabase'
 
 
-@app.route('/')
-def get(user_id):
+class Accounts(Resource):
+    def post(self,user_id):
         # results = Account.query.all()
         # res=[]
         results = Account.query.filter_by(UserID = user_id).all()
@@ -24,12 +24,12 @@ def get(user_id):
             result_dict.pop('_sa_instance_state', None)
             res.append(result_dict)
 
-        return render_template("index.html",accounts =res)
+        return jsonify(res)
 
-
+api.add_resource(Accounts,'/account/<int:user_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=8000)
 
 
 
