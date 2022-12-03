5cf655884a094bf06246a3079cb5aca960a5795d
Ang Wei Feng (Ted) <aweifeis@gmail.com>
2022-12-03 13:29:45 +0800

    generate jwt

--- spring-backend/src/main/java/com/dbstt19/dbs_backend/controller/AuthController.java
+++ spring-backend/src/main/java/com/dbstt19/dbs_backend/controller/AuthController.java
@@ -1,18 +1,24 @@
 package com.dbstt19.dbs_backend.controller;
 
+import com.dbstt19.dbs_backend.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthController {
+    private final JwtService jwtService;
 
     @GetMapping("/")
-    public String greet(){
+    public String greet() {
         return "Hello world";
     }
 
     @GetMapping("/token")
-    public String getToken(){
-        return "Make this a post request to get token";
+    public String getToken(Authentication authentication) {
+        String token = jwtService.generateJwt(authentication);
+        return token;
     }
 }
