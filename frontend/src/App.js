ea1ccc4cad1c3b21ddd25b467dbcae238bc00d42
jonahtanjz <jonahtanjz8@gmail.com>
2022-12-03 13:05:53 +0800

    frontend: add navigation menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <BrowserRouter>
         <Routes>
           <Route exact path="/login" element={<Login />} />
