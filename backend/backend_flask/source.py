0dd99ff22b24f170f5b6c4b4012194fb34c87294
yuqi <111860394+threalyn@users.noreply.github.com>
2022-12-03 16:05:21 +0800

    Update source.py

--- backend/backend_flask/source.py
+++ backend/backend_flask/source.py
@@ -1,4 +1,6 @@
-from flask import Flask, redirect, request, render_template, redirect,url_for, flash,session
+from flask import Flask, redirect, request, render_template, redirect,url_for, flash,session,jsonify
+from flask_wtf import FlaskForm
+from flask_wtf.file import  FileRequired
 from accounts import Account
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
@@ -11,17 +13,19 @@ api = Api(app)
 
 
 @app.route('/')
-def get_all():
-    results = Account.query.all()
-    res = dict()
-    res["AccountID"] = 
-       # return render_template('index.html', accounts = self.AccountID)
-    return render_template("index.html",accounts = results)
+def get(user_id):
+        # results = Account.query.all()
+        # res=[]
+        results = Account.query.filter_by(UserID = user_id).all()
+        res = []
+        # return render_template('index.html', accounts = self.AccountID)
+        for result in results:
+            result_dict = result.__dict__
+            result_dict.pop('_sa_instance_state', None)
+            res.append(result_dict)
 
+        return render_template("index.html",accounts =res)
 
-def get_by_userID():
-    results = Account.query.all()
-    return render_template("index.html",accounts = results)
 
 
 if __name__ == '__main__':
