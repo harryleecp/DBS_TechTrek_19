d741f0bf9b9cd4388ab081eed4b08128eb604e44
vanessa <vleungz.98@gmail.com>
2022-12-03 12:57:28 +0800

    Transaction table v1

--- frontend/src/pages/Transaction.js
+++ frontend/src/pages/Transaction.js
@@ -6,17 +6,30 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
-
-const rows = [{id: '1', name: 'name'}]
+import axios from "axios";
 
 export default class Transaction extends React.Component {
     componentDidMount() {
-
+        getTransactions()
     }
 
     state = {
-
+        accountId: '12345',
+        transactions: [{
+            "TransactionID": 1,
+            "AccountID": 958945214,
+            "ReceivingAccountID": 621156213,
+            "Date": "2022-11-08T04:00:00.000Z",
+            "TransactionAmount": 8996.00,
+            "Comment": "School Fees"
+        },{
+            "TransactionID": 2,
+            "AccountID": 958945214,
+            "ReceivingAccountID": 12384222,
+            "Date": "2022-11-09T04:00:00.000Z",
+            "TransactionAmount": 1000.00,
+            "Comment": "Transport Fees"
+        }]
     }
 
     render() {
@@ -26,20 +39,21 @@ export default class Transaction extends React.Component {
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
                         <TableHead>
                             <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell align="right">ID</TableCell>
+                                <TableCell>Date</TableCell>
+                                <TableCell>Transaction ID</TableCell>
+                                <TableCell align="right">Receiving Account ID</TableCell>
+                                <TableCell align="right">Transaction Amount</TableCell>
+                                <TableCell align="right">Type</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {rows.map((row) => (
-                                <TableRow
-                                    key={row.name}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                    <TableCell component="th" scope="row">
-                                        {row.name}
-                                    </TableCell>
-                                    <TableCell align="right">{row.id}</TableCell>
+                            {this.state.transactions.map((row) => (
+                                <TableRow key={row.TransactionID}>
+                                    <TableCell component="th" scope="row">{row.Date}</TableCell>
+                                    <TableCell component="th" scope="row">{row.TransactionID}</TableCell>
+                                    <TableCell align="right">{row.ReceivingAccountID}</TableCell>
+                                    <TableCell align="right">{row.TransactionAmount}</TableCell>
+                                    <TableCell align="right">{row.Comment}</TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
@@ -49,3 +63,10 @@ export default class Transaction extends React.Component {
         )
     }
 }
+
+const getTransactions = async () =>
+    await axios.post('/dashboard', {accountId: this.state.accountId})
+        .then((res) => {
+
+            this.setState({transactions: res})
+        })
