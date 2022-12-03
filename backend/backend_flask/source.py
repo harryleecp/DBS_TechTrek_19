b308a4ca3c74d3264a6b67009ebec106156b9d86
yuqi <111860394+threalyn@users.noreply.github.com>
2022-12-03 16:57:56 +0800

    Update source.py

--- backend/backend_flask/source.py
+++ backend/backend_flask/source.py
@@ -3,7 +3,7 @@ from accounts import Account
 from user import User
 from transactions import Transaction
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, abort, reqparse
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:letmeinpls@test-1.c6htqhzutcbg.ap-northeast-1.rds.amazonaws.com:3306/Bank-flask'
@@ -38,6 +38,14 @@ class Users(Resource):
 
 api.add_resource(Users,'/user/<int:user_id>')
 
+put_args = reqparse.RequestParser()
+put_args.add_argument("TransactionID", type=int, required=True)
+put_args.add_argument("AccountID", type=int, required=True)
+put_args.add_argument("ReceivingAccountID", type=int, required=True)
+put_args.add_argument("Date", type=int, required=True)
+put_args.add_argument("TransactionAmount", type=int, required=True)
+put_args.add_argument("Comment", type=int, required=True)
+
 class Transactions(Resource):
     def post(self,account_id):
         results = Transactions.query.filter_by(AccountID = account_id).all()
@@ -50,9 +58,27 @@ class Transactions(Resource):
 
         return jsonify(res)
 
-    def add_transactions():
+    def add_transactions(self, transaction_id):
+        args = put_args.parse_args()
+        result = Transactions.query.filter_by(id=transaction_id).first()
+        if result:
+            abort(409, message="Video id taken...")
+            
+        transact = Transactions(TransactionID='TransactionID', AccountID: args["AccountID"], ReceivingAccountID: args["ReceivingAccountID"], 
+        Date: args["Date"], TransactionAmount: args["TransactionAmount"], Comment: args["Comment"])
+        
+        db.session.add(transact)
+        db.session.commit()
 
-        return
+        results = Transactions.query.filter_by(TransactionID = transaction_id).all()
+        res = []
+
+        for result in results:
+            result_dict = result.__dict__
+            result_dict.pop('_sa_instance_state', None)
+            res.append(result_dict)
+
+        return jsonify(res)
 
     def delete_transactions(self, transaction_id):
         Transaction.query.filter(TransactionID = transaction_id).delete()
